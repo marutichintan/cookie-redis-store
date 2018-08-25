c9ec234ed90017270f3aa2441fd602cf9a6757ef
Caio Flores <caio@mytech.com.br>
2018-08-25 18:14:57 -0400

    tought-cookie-redis-store

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "mocha": true
+    },
+    "rules": {
+        "no-console": ["error", { allow: ["warn", "error"] }]
+    }
+};
